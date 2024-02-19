79c0b45f71b129f08734aa16ba75892ff5600da4
Ergo <ergo.raidma@tktk.ee>
2024-02-19 23:04:46 +0200

    creat new app

---
+++ Week6RandomElement/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+string[] snacks = { "sushi", "pizza", "burger", "chiken wings", "Asian takeaway" };
+
+Random rnd = new Random();
+
+int RandomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[RandomIndex]}.");
---
+++ Week6StringAndArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+
+string hello = " we are the chempions?";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+
+Console.Write(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.Write(firstLetter);
+
+Console.WriteLine(hello);
+
+//int wordCounter = 1;
+//for(int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i])) 
+//    {
+//    wordCounter++;
+//    }
+//}
+ 
+//Console.WriteLine($"Word counter in {hello}: {wordCounter} word.");
